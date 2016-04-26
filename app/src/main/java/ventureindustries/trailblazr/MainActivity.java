b2551ac81328fe9a65610acf9334ece2e503647e
Neal Friedman <nealiof1000@gmail.com>
2016-04-26 13:03:04 -0600

    initial commit

---
+++ app/src/main/java/ventureindustries/trailblazr/MainActivity.java
@@ -0,0 +1,13 @@
+package ventureindustries.trailblazr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
